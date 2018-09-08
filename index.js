a878e91c234d1ad05b9cd85e628181c0d829bb8f
Nexos2st <43100593+Nexos2st@users.noreply.github.com>
2018-09-09 00:58:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login("process.env.TOKEN")
+bot.login(process.env.TOKEN)
 var prefix = ("?")
 
 bot.on('ready', function() {
